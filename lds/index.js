43dbcb7bd31b131fc158c66a8852ee2ee03fdb06
Fredrik Söderquist <fregu808@gmail.com>
2016-06-22 08:51:22 +0200

    Let @daytona/living-design-system do the heavy lifting of lds command and starting server, as alone global dependency

--- lds/index.js
+++ lds/index.js
@@ -1,15 +1,78 @@
-var cli = require('lds-cli');
-var server = require('lds-server');
-var build = require('lds-build');
-var styleguide = require('lds-styleguide');
-var test = require('lds-test');
-var api = require('lds-api');
+#!/usr/bin/env node --use_strict
 
-module.exports = {
+var fs = require('fs');
+var fileExists = require('file-exists');
+var path = require('path');
+var cli = require('@daytona/lds-cli');
+var server = require('@daytona/lds-server');
+var generator = require('@daytona/lds-create');
+var build = require('@daytona/lds-build');
+var styleguide = require('@daytona/lds-styleguide');
+var test = require('@daytona/lds-test');
+var api = require('@daytona/lds-api');
+var engine = require('@daytona/lds-engine');
+var parser = require('@daytona/lds-parser');
+var config = process.cwd() && fileExists(process.cwd() + '/lds.config.js') ? require(process.cwd() + '/lds.config') : false;
+
+var dependencies = {
   cli,
   build,
   server,
   styleguide,
   test,
   api,
+  parser,
+  engine : engine,
 };
+
+function setup(config) {
+  cli({
+    start: {
+      description: "Start new HTTP-server to serve views and styleguide",
+      action() {
+        return server(config);
+      }
+    },
+    build: {
+      description: "Build all assets to dist folder, starting watchtasks",
+      action(type = '') {
+        console.log('build assets');
+        return build(type, config);
+      }
+    },
+    create: {
+      description: "Create new component,view,base,helper,layout",
+      action(type, name) {
+        generator.create(type, name, config);
+      }
+    },
+    test: {
+      description: "Test your LDS strucuture, file syntax and all nessecary functions",
+      action(type = 'all') {
+        console.log('Tests not yet implemented. sorry');
+      }
+    }
+  });
+}
+
+// If run from CLI
+if (process.argv && process.cwd()) {
+  var root = process.cwd(); // Command line directory
+  if (config) {
+    setup(config);
+  } else {
+    // IF no config is available, only allow '$ lds init'
+    cli({
+      init: {
+        description: "Initialize a blank direcory as a LDS seting up a default folder structure",
+        action() {
+          return generator.init(root, function() {
+            console.log('A new living design system is set up');
+          });
+        }
+      }
+    });
+  }
+}
+
+module.exports = dependencies;
