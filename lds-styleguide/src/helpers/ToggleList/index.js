99c691268f5b1fdd4c84ef0550aea1f90b1d924c
Fredrik Söderquist <fregu808@gmail.com>
2016-12-22 12:50:45 +0100

    Enabling listtogglebuttons

---
+++ lds-styleguide/src/helpers/ToggleList/index.js
@@ -0,0 +1,30 @@
+import controller from '../controller';
+
+function ToggleList(el) {
+  const togglerButtonGrid = el.querySelector('.js-toggler-grid');
+  const togglerButtonList = el.querySelector('.js-toggler-list');
+  const toggleList = el.querySelector('.js-toggleList');
+
+  const gridClass = 'ComponentList--grid';
+  const listClass = 'ComponentList--listed';
+
+  function toggleListFormat (format) {
+    toggleList.classList.remove(gridClass, listClass);
+    if (format === 'list') {
+      toggleList.classList.add(listClass);
+    } else {
+      toggleList.classList.add(gridClass);
+    }
+  }
+
+  function init () {
+    console.log('init toggleList', togglerButtonGrid, togglerButtonList, toggleList);
+    if (togglerButtonGrid && togglerButtonList && toggleList) {
+      togglerButtonGrid.addEventListener('click', () => toggleListFormat('grid'));
+      togglerButtonList.addEventListener('click', () => toggleListFormat('list'));
+    }
+  }
+  return { init }
+}
+
+controller.add('ToggleList', ToggleList);
